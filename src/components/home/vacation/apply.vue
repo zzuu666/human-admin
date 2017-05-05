ebf409a41cd5aeb15a6bea0a69cecf96b81a7147
Sawyer <zzuu666@outlook.com>
2017-05-05 17:07:24 +0800

    改用默认浏览器开启

--- src/components/home/vacation/apply.vue
+++ src/components/home/vacation/apply.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="apply">
-    {{vacation}}
+    <Row>
+      <Col class="apply-header" span="14" offset="5">
+        您的年假剩余 {{vacation}} 天
+      </Col>
+      <Col span="14" offset="5">
+        <Form :label-width="80">
+          <Form-item label="选择日期">
+            <Row>
+              <Col span="11">
+                <Date-picker type="date" placeholder="开始日期"></Date-picker>
+              </Col>
+              <Col span="2" style="text-align: center">-</Col>
+              <Col span="11">
+                <Date-picker type="date" placeholder="结束日期"></Date-picker>
+              </Col>
+            </Row>
+          </Form-item>
+          <Form-item>
+            <Button type="primary">提交</Button>
+          </Form-item>
+        </Form>
+      </Col>
+    </Row>
+    
   </div>
 </template>
 
@@ -21,22 +44,11 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+<style lang="less" scoped>
+.apply {
+  .apply-header {
+    margin-bottom: 20px;    
+    font-size: 18px;
+  }
 }
 </style>
